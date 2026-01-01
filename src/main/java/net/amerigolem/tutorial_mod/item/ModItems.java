77df43451ba41167e579af2741de8b93f30ad270
AmeriGolem <aberlenghi@gmail.com>
2026-01-01 22:35:52 +0100

    add food and burnable example

--- src/main/java/net/amerigolem/tutorial_mod/item/ModItems.java
+++ src/main/java/net/amerigolem/tutorial_mod/item/ModItems.java
@@ -5,6 +5,7 @@ import net.amerigolem.tutorial_mod.item.custom.ChiselItem;
 import net.amerigolem.tutorial_mod.item.custom.DashWand;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -19,6 +20,10 @@ public class ModItems {
     public static final Item CHISEL = registerItem("chisel", new ChiselItem(new Item.Settings().maxDamage(32)));
     public static final Item DASH_WAND = registerItem("dash_wand", new DashWand(new Item.Settings().maxDamage(32)));
 
+    public static final Item CAULIFLOWER = registerItem("cauliflower", new Item(new Item.Settings().food(ModFoodComponents.CAULIFLOWER)));
+
+    public static final Item STARLIGHT_ASHES = registerItem("starlight_ashes", new Item(new Item.Settings()));
+
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
     }
@@ -31,6 +36,12 @@ public class ModItems {
             entries.add(RAW_PINK_GARNET);
             entries.add(CHISEL);
             entries.add(DASH_WAND);
+            entries.add(CAULIFLOWER);
+            entries.add(STARLIGHT_ASHES);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.FOOD_AND_DRINK).register( entries -> {
+            entries.add(CAULIFLOWER);
         });
     }
 }
