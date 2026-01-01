1cb62b748e2d62f15ab4af51b15329cc173d2d67
AmeriGolem <aberlenghi@gmail.com>
2026-01-01 15:08:44 +0100

    adding bricks and changing the item groups adding

--- src/main/java/net/amerigolem/tutorial_mod/block/ModBlocks.java
+++ src/main/java/net/amerigolem/tutorial_mod/block/ModBlocks.java
@@ -1,12 +1,12 @@
 package net.amerigolem.tutorial_mod.block;
 
 import net.amerigolem.tutorial_mod.TutorialMod;
+import net.amerigolem.tutorial_mod.items.ModItemGroups;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
@@ -20,6 +20,9 @@ public class ModBlocks {
 
     public static final Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block",
             new Block(AbstractBlock.Settings.create().strength(3f).requiresTool().sounds(BlockSoundGroup.AMETHYST_CLUSTER)));
+
+    public static final Block CYAN_BRICK_BLOCK = registerBlock("cyan_brick_block",
+            new Block(AbstractBlock.Settings.create().strength(5f).requiresTool().sounds(BlockSoundGroup.NETHER_BRICKS)));
 
 
     private static Block registerBlock(String name, Block block) {
@@ -35,9 +38,10 @@ public class ModBlocks {
     public static void registerModBlocks() {
         TutorialMod.LOGGER.info("Registering Mod Blocks for " + TutorialMod.MOD_ID);
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
+        ItemGroupEvents.modifyEntriesEvent(ModItemGroups.PINK_GARNET_BLOCK_GROUP_KEY).register(entries -> {
             entries.add(PINK_GARNET_BLOCK);
             entries.add(RAW_PINK_GARNET_BLOCK);
+            entries.add(CYAN_BRICK_BLOCK);
         });
 
     }
--- src/main/java/net/amerigolem/tutorial_mod/items/ModItems.java
+++ src/main/java/net/amerigolem/tutorial_mod/items/ModItems.java
@@ -3,7 +3,6 @@ package net.amerigolem.tutorial_mod.items;
 import net.amerigolem.tutorial_mod.TutorialMod;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -23,7 +22,7 @@ public class ModItems {
     public static void registerModItems() {
         TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
 
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register( entries -> {
+        ItemGroupEvents.modifyEntriesEvent(ModItemGroups.PINK_GARNET_GROUP_KEY).register( entries -> {
             entries.add(PINK_GARNET);
             entries.add(RAW_PINK_GARNET);
         });
