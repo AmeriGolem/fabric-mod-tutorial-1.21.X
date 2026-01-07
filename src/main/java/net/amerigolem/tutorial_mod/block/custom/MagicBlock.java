5077f77d0458f6291d8201bdf253019fe4fd2d12
AmeriGolem <aberlenghi@gmail.com>
2026-01-07 01:39:31 +0100

    add tags

--- src/main/java/net/amerigolem/tutorial_mod/block/custom/MagicBlock.java
+++ src/main/java/net/amerigolem/tutorial_mod/block/custom/MagicBlock.java
@@ -1,19 +1,24 @@
 package net.amerigolem.tutorial_mod.block.custom;
 
-import net.amerigolem.tutorial_mod.item.ModItems;
+import net.amerigolem.tutorial_mod.util.ModTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.item.tooltip.TooltipType;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+
+import java.util.List;
 
 public class MagicBlock extends Block {
     public MagicBlock(Settings settings) {
@@ -29,14 +34,26 @@ public class MagicBlock extends Block {
     }
 
 
+
     @Override
     public void onSteppedOn(World world, BlockPos pos, BlockState state, Entity entity) {
         if(entity instanceof ItemEntity itemEntity) {
-            if(itemEntity.getStack().getItem() == ModItems.RAW_PINK_GARNET) {
+            if(isValidItem(itemEntity.getStack())) {
                 itemEntity.setStack(new ItemStack(Items.DIAMOND, itemEntity.getStack().getCount()));
             }
         }
 
         super.onSteppedOn(world, pos, state, entity);
+    }
+
+    private boolean isValidItem(ItemStack stack) {
+        return stack.isIn(ModTags.Items.TRANSFORMABLE_ITEMS);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, Item.TooltipContext context, List<Text> tooltip, TooltipType options) {
+        tooltip.add(Text.translatable("tooltip.tutorialmod.magic_block.tooltip"));
+        tooltip.add(Text.translatable("tooltip.tutorialmod.magic_block.tooltip2"));
+        super.appendTooltip(stack, context, tooltip, options);
     }
 }
