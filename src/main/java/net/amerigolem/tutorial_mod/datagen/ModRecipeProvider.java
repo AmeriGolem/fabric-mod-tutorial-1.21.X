0a779613eeb2f9de502987f32b60e60c7816d12b
AmeriGolem <aberlenghi@gmail.com>
2026-01-07 14:36:16 +0100

    Added Data generation

---
+++ src/main/java/net/amerigolem/tutorial_mod/datagen/ModRecipeProvider.java
@@ -0,0 +1,78 @@
+package net.amerigolem.tutorial_mod.datagen;
+
+import net.amerigolem.tutorial_mod.TutorialMod;
+import net.amerigolem.tutorial_mod.block.ModBlocks;
+import net.amerigolem.tutorial_mod.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
+import net.minecraft.advancement.criterion.InventoryChangedCriterion;
+import net.minecraft.data.server.recipe.RecipeExporter;
+import net.minecraft.data.server.recipe.ShapedRecipeJsonBuilder;
+import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
+import net.minecraft.item.ItemConvertible;
+import net.minecraft.item.Items;
+import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.util.Identifier;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeProvider extends FabricRecipeProvider {
+    public ModRecipeProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    public void generate(RecipeExporter recipeExporter) {
+        List<ItemConvertible> PINK_GARNET_SMELTABLES = List.of(ModItems.RAW_PINK_GARNET, ModBlocks.PINK_GARNET_ORE,
+                ModBlocks.PINK_GARNET_DEEPSLATE_ORE);
+
+        offerSmelting(recipeExporter, PINK_GARNET_SMELTABLES, RecipeCategory.MISC, ModItems.PINK_GARNET, 0.25f, 200, "pink_garnet");
+        offerBlasting(recipeExporter, PINK_GARNET_SMELTABLES, RecipeCategory.MISC, ModItems.PINK_GARNET, 0.25f, 100, "pink_garnet");
+
+        offerReversibleCompactingRecipes(recipeExporter, RecipeCategory.BUILDING_BLOCKS, ModItems.PINK_GARNET, RecipeCategory.DECORATIONS, ModBlocks.PINK_GARNET_BLOCK);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModBlocks.RAW_PINK_GARNET_BLOCK)
+                .pattern("RRR")
+                .pattern("RRR")
+                .pattern("RRR")
+                .input('R', ModItems.RAW_PINK_GARNET)
+                .criterion(hasItem(ModItems.RAW_PINK_GARNET), conditionsFromItem(ModItems.RAW_PINK_GARNET))
+                .offerTo(recipeExporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.DASH_WAND)
+                .pattern("P")
+                .pattern("R")
+                .input('P', Items.ENDER_PEARL)
+                .input('R', Items.BLAZE_ROD)
+                .criterion("has_Ender_Pearl_or_Blaze_Rod",
+                        InventoryChangedCriterion.Conditions.items(
+                                Items.ENDER_PEARL, Items.BLAZE_ROD
+                        )
+                )
+                .offerTo(recipeExporter);
+
+        ShapedRecipeJsonBuilder.create(RecipeCategory.BUILDING_BLOCKS, ModBlocks.CYAN_BRICK_BLOCK)
+                .pattern(" b ")
+                .pattern("bdb")
+                .pattern(" b ")
+                .input('b', Items.BRICK)
+                .input('d', Items.CYAN_DYE)
+                .criterion("has_brick_or_dye",
+                        InventoryChangedCriterion.Conditions.items(
+                                Items.BRICK, Items.CYAN_DYE
+                        ))
+                .offerTo(recipeExporter);
+
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.RAW_PINK_GARNET, 9)
+                .input(ModBlocks.RAW_PINK_GARNET_BLOCK)
+                .criterion(hasItem(ModBlocks.RAW_PINK_GARNET_BLOCK), conditionsFromItem(ModBlocks.RAW_PINK_GARNET_BLOCK))
+                .offerTo(recipeExporter);
+
+        ShapelessRecipeJsonBuilder.create(RecipeCategory.MISC, ModItems.RAW_PINK_GARNET, 32)
+                .input(ModBlocks.MAGIC_BLOCK)
+                .criterion(hasItem(ModBlocks.RAW_PINK_GARNET_BLOCK), conditionsFromItem(ModBlocks.RAW_PINK_GARNET_BLOCK))
+                .offerTo(recipeExporter, Identifier.of(TutorialMod.MOD_ID,"raw_pink_garnet_from_magic_block"));
+    }
+}
